611bf3c4c808394c67b9ff5bff3e07294d717d2d
Rocío Beato Martín <Rociobeato91@gmail.com>
2021-03-28 14:14:53 +0200

    Primera subida

--- Persona.ts
+++ Persona.ts
@@ -1,8 +1,8 @@
-import { Direccion } from "./Direccion";
-import { Mail } from "./Mail";
+import{ Direccion } from "./Direccion";
+import{ Mail } from "./Mail";
 import { Telefono } from "./Telefono";
 
-export class Persona {
+export class Persona{
     private nombre: string;
     private apellidos: string;
     private edad: number;
@@ -15,18 +15,78 @@ export class Persona {
     private telefonos: number[];
     private notas: string;
 
-    constructor(nombre: string, apellidos: string, edad: number, dni: string, cumpleanios: Date, color: string,
-        sexo: string, direcciones: string[], mails: string[], telefonos: number[], notas: string) {
+    SetNombre(nombre: string) {
         this.nombre = nombre;
-        this.apellidos = apellidos;
-        this.edad = edad;
-        this.dni = dni;
-        this.cumpleanios = cumpleanios;
-        this.color = color;
-        this.sexo = sexo;
-        this.direcciones = direcciones;
-        this.mails = mails;
-        this.telefonos = telefonos;
-        this.notas = notas;
     }
+    GetNombre(nombre:string){
+        return this.nombre;
+    }
+    SetApellidos(apellidos: string){
+        this.apellidos=apellidos;
+    }
+    GetApellidos(apellidos:string){
+        return this.apellidos;
+    }
+    SetEdad(edad:number){
+        this.edad=edad;
+    }
+    GetEdad(edad:number){
+        return this.edad;
+    }
+    SetDni(dni:string){
+        this.dni=dni;
+    }
+    GetDni(dni:string){
+        return this.dni;
+    }
+    SetCumpleanios(cumpleanios:Date){
+        this.cumpleanios=cumpleanios;
+    }
+    GetCumpleanios(cumpleanios:Date){
+        return this.cumpleanios;
+    }
+    SetColor(color:string){
+        this.color=color;
+    }
+    GetColor(color:string){
+        return this.color;
+    }
+    SetSexo(sexo:string){
+        this.sexo=sexo;
+    }
+    GetSexo(sexo:string){
+        return this.sexo;
+    }
+    SetDirecciones(direcciones:string[]){
+        this.direcciones=direcciones;
+    }
+    GetDirecciones(direcciones:string[]){
+        return this.direcciones;
+    }
+    SetMails(mails:string[]){
+        this.mails=mails;
+    }
+    GetMails(mails:string[]){
+        return this.mails;
+    }
+    SetTelefonos(telefonos:number[]){
+        this.telefonos=telefonos;
+    }
+    GetTelefonos(telefonos:number[]){
+        return this.telefonos;
+    }
+    SetNotas(notas:string){
+        this.notas=notas;
+    }
+    GetNotas(notas:string){
+        return this.notas;
+    }
+
+    
+
+
+
+
+
+
 }
